86334f2fc4ebb111633555fb913c8f808a556b76
Frederic <frederic.t.chan@gmail.com>
2018-01-18 22:25:49 +0800

    remove default config
    Signed-off-by: Frederic <frederic.t.chan@gmail.com>

--- src/everyclass/config/__init__.py
+++ src/everyclass/config/__init__.py
@@ -3,16 +3,13 @@ import os
 
 def load_config():
     mode = os.environ.get('MODE')
-    try:
-        if mode == 'PRODUCTION':
-            from .production import ProductionConfig
-            return ProductionConfig
-        elif mode == 'DEVELOPMENT':
-            from .development import DevelopmentConfig
-            return DevelopmentConfig
-        elif mode == 'STAGING':
-            from .staging import StagingConfig
-            return StagingConfig
-    except ImportError:
-        from .default import Config
-        return Config
+
+    if mode == 'PRODUCTION':
+        from .production import ProductionConfig
+        return ProductionConfig
+    elif mode == 'DEVELOPMENT':
+        from .development import DevelopmentConfig
+        return DevelopmentConfig
+    elif mode == 'STAGING':
+        from .staging import StagingConfig
+        return StagingConfig
