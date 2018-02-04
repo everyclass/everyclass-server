b1028dbebc0e81f9cea819bbe18b81c23db51959
Frederic <frederic.t.chan@gmail.com>
2018-02-04 19:59:06 +0800

    change deploy script
    Signed-off-by: Frederic <frederic.t.chan@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'Connecting to production server...'
-ssh travis@admirable.one
+ssh travis@admirable.one -o PubkeyAuthentication=no
 
 echo 'cd to dir'
 cd /home/pyweb/EveryClass-server
