5aa01a84d8f13810fc9bc50b64215ba5c8348655
Frederic <frederic.t.chan@gmail.com>
2018-02-06 10:50:17 +0800

    travis
    Signed-off-by: Frederic <frederic.t.chan@gmail.com>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 set -xe
 
-eval "$(ssh-agent -s)"
-chmod 600 /tmp/deploy_rsa
-ssh-add /tmp/deploy_rsa
-
-
-
-if [ $TRAVIS_BRANCH = "master" ] ; then
-
-    ssh travis@admirable.one <<EOF
-cd /home/pyweb/EveryClass-server
-git pull
-EOF
-
-
-else
-
-     echo "No deploy script for branch '$TRAVIS_BRANCH'. Skip deploying."
-
-fi
