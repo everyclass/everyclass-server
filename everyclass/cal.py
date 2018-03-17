59bf83c78d9e14c86f178f5cde77b6fb174e392a
Frederic <frederic.t.chan@gmail.com>
2018-03-17 16:18:53 +0800

    changelog
    Signed-off-by: Frederic <frederic.t.chan@gmail.com>

--- everyclass/cal.py
+++ everyclass/cal.py
@@ -17,6 +17,7 @@ def cal_page():
     from .db_operations import get_classes_for_student, get_my_semesters, check_if_stu_exist
     from .model import Semester
     from . import ics_generator
+    from . import access_log
 
     # 如果请求中包含 id 就写入 session
     if request.values.get('id'):
@@ -27,6 +28,9 @@ def cal_page():
 
     # 如果 session 中有 stu_id 就生成 ics 并返回页面，没有就跳转回首页
     if session.get('stu_id', None):
+        # async access log
+        access_log.delay(op_type='q_export', stu_id=session['stu_id'])
+
         # 获得学生姓名和他的合法学期
         my_available_semesters, student_name = get_my_semesters(session['stu_id'])
         semester = Semester.get()
