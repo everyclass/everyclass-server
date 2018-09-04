9a00c3778bd1668d5ce3e7dd73d5948fd2ae35e0
Frederic <frederic.t.chan@gmail.com>
2018-09-04 19:50:18 +0800

    rename travis.sh and modify it
    Signed-off-by: Frederic <frederic.t.chan@gmail.com>

--- deploy/travis.sh
+++ deploy/travis.sh
@@ -13,14 +13,19 @@ ssh-add /tmp/stage_key
 
 if [ $TRAVIS_BRANCH = "master" ] ; then
 
-# pip install pipenv
-#pipenv clean
-#pipenv install
 ssh -o StrictHostKeyChecking=no travis@every.admirable.one <<EOF
-cd /var/EveryClass-server
-git reset --hard
-git pull
-touch reload
+cd /var/EveryClass-server && \
+    git reset --hard && \
+    git pull && \
+    docker build -t fr0der1c/everyclass-server . && \
+    docker run -it --rm -d \
+        --net=host \
+        --name "everyclass-`git describe`" \
+        -v "`pwd`/everyclass/config:/var/everyclass-server/config" \
+        -v "`pwd`/calendar_files:/var/everyclass-server/calendar_files" \
+        -e UWSGI_HTTP_SOCKET=":9000" \
+        fr0der1c/everyclass-server && \
+    wait 30s, replace nginx upstream with new container, and stop old container.
 EOF
 
 elif [ $TRAVIS_BRANCH = "develop" ] ; then
