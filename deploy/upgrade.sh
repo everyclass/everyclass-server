899b274b56caf51610085cea1dea263e37a720cd
Frederic Chan <frederic.t.chan@gmail.com>
2018-09-24 19:11:55 +0800

    update script
    Signed-off-by: Frederic Chan <frederic.t.chan@gmail.com>

--- deploy/upgrade.sh
+++ deploy/upgrade.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Upgrade script for EveryClass
+# Upgrade script for Admirable applications
 #
 # - Get latest source code
 # - Build Docker image (use `--no-build` option to skip building)
@@ -9,6 +9,10 @@
 
 # exit if any command fails
 set -e
+
+# Configurations goes here
+APP_DOCKER_REPO="fr0der1c/everyclass-server"
+APP_CONTAINER_BASENAME="everyclass"
 
 # generate a unused port
 function EPHEMERAL_PORT(){
@@ -26,15 +30,16 @@ function EPHEMERAL_PORT(){
 }
 
 
-EVERYCLASS_PORT=$(EPHEMERAL_PORT)
-EVERYCLASS_START_TIMEOUT=30
-EVERYCLASS_URL=http://localhost:$(echo ${EVERYCLASS_PORT})
+APP_PORT=$(EPHEMERAL_PORT)
+APP_START_TIMEOUT=30
+APP_URL=http://localhost:$(echo ${APP_PORT})
 
 
 ### Build and run image
 
 # get latest source code and build Docker image
-cd /var/EveryClass-server
+cd `dirname $0` # cd to deploy/
+cd ../ # cd to project root (where Dockerfile exists)
 if [ "$1" != "--rollback" ]
 then
     git reset --hard
@@ -44,36 +49,36 @@ else
 fi
 if [ "$1" != "--no-build" ]
 then
-    docker build -t fr0der1c/everyclass-server .
+    docker build -t ${APP_DOCKER_REPO} .
 fi
 
 # run Docker image
 docker run -it --rm -d \
     --net=host \
-    --name "everyclass-$(git describe --tags)-$(date "+%m%d-%H%M")" \
+    --name "${APP_CONTAINER_BASENAME}-$(git describe --tags)-$(date "+%m%d-%H%M")" \
     -v "$(pwd)/everyclass/server/config:/var/everyclass-server/everyclass/server/config" \
     -v "$(pwd)/calendar_files:/var/everyclass-server/calendar_files" \
-    -p $(echo ${EVERYCLASS_PORT}):$(echo ${EVERYCLASS_PORT}) \
-    -e UWSGI_HTTP_SOCKET=":$(echo ${EVERYCLASS_PORT})" \
-    fr0der1c/everyclass-server
+    -p $(echo ${APP_PORT}):${APP_PORT} \
+    -e UWSGI_HTTP_SOCKET=":${APP_PORT}" \
+    ${APP_DOCKER_REPO}
 
 
 ### Roll-upgrade
-LATEST_CONTAINER=$(docker ps -q --filter name=everyclass -l)
-ALL_CONTAINERS=$(docker ps -q --filter name=everyclass)
+LATEST_CONTAINER=$(docker ps -q --filter name=${APP_CONTAINER_BASENAME} -l)
+ALL_CONTAINERS=$(docker ps -q --filter name=${APP_CONTAINER_BASENAME})
 
 # wait 30 seconds, replace nginx upstream with new container, and stop old container.
 counter=0
-while [ ! "$(curl -k ${EVERYCLASS_URL} 2> /dev/null)" -a ${counter} -lt ${EVERYCLASS_START_TIMEOUT}  ]; do
+while [ ! "$(curl -k ${APP_URL} 2> /dev/null)" -a ${counter} -lt ${APP_START_TIMEOUT}  ]; do
     sleep 1
     ((counter++))
-    echo "waiting for EveryClass to be up ($counter/$EVERYCLASS_START_TIMEOUT)"
+    echo "waiting for application to be up ($counter/$APP_START_TIMEOUT)"
 done
-if [ ! "$(curl -k ${EVERYCLASS_URL} 2> /dev/null)" ]; then
-    echo "Couldn't start EveryClass. Exiting."
+if [ ! "$(curl -k ${APP_URL} 2> /dev/null)" ]; then
+    echo "Couldn't start application. Exiting."
     exit 1
 fi
-echo "EveryClass started."
+echo "Application started."
 
 # stop old containers (stop nothing if there are no old containers)
 for each in ${ALL_CONTAINERS[@]}
