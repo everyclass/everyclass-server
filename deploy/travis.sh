baf2db3dd90fecd0a7c00bca33ad01d12369196c
Frederic Chan <frederic.t.chan@gmail.com>
2018-10-19 16:51:44 +0800

    build image and do not auto deploy anymore
    Signed-off-by: Frederic Chan <frederic.t.chan@gmail.com>

--- deploy/travis.sh
+++ deploy/travis.sh
@@ -3,37 +3,24 @@
 
 set -xe
 
-eval "$(ssh-agent -s)"
-chmod 600 /tmp/deploy_key
-chmod 600 /tmp/stage_key
-ssh-add /tmp/deploy_key
-ssh-add /tmp/stage_key
-
-
-
 if [ $TRAVIS_BRANCH = "release" ] ; then
 
 # checkout release first, otherwise `git describe` will be wrong
 git checkout release
 VERSION=$(git describe --tags)
+
+# sentry release
 curl -sL https://sentry.io/get-cli/ | bash
 export SENTRY_ORG=admirable
 export SENTRY_URL=https://sentry.admirable.pro/
 sentry-cli releases new -p everyclass-server --finalize "$VERSION"
 sentry-cli releases set-commits "$VERSION" --auto
-DEPLOY_START_TIME=$(date +%s)
 
-ssh -o StrictHostKeyChecking=no travis@every.admirable.one <<EOF
-cd /var/EveryClass-server
-echo "Reset git repository..."
-git reset --hard
-echo "Pulling latest code..."
-git pull
-bash deploy/upgrade.sh
-EOF
-
-DEPLOY_END_TIME=$(date +%s)
-sentry-cli releases deploys ${VERSION} new -e production -t $((DEPLOY_END_TIME-DEPLOY_START_TIME))
+# build Docker image and upload to hub
+docker login --username ${DOCKER_USERNAME} ccr.ccs.tencentyun.com --password ${DOCKER_PASSWORD}
+IMAGE_ADDRESS=ccr.ccs.tencentyun.com/everyclass/everyclass-server:${VERSION}
+docker build -t ${IMAGE_ADDRESS}
+docker push ${IMAGE_ADDRESS}
 
 else
 
