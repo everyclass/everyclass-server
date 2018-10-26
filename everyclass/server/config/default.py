9419f6448bbc655d8d218ba108c465f8c97fa63a
Frederic <frederic.t.chan@gmail.com>
2018-10-26 16:23:29 +0800

    no stack send to elastic apm
    Signed-off-by: Frederic <frederic.t.chan@gmail.com>

--- everyclass/server/config/default.py
+++ everyclass/server/config/default.py
@@ -19,15 +19,37 @@ class Config(object):
         'use_unicode': True,
         'charset'    : 'utf8mb4'
     }
+
+    """
+    Git Hash
+    """
+    _git_repo = git.Repo(search_parent_directories=True)
+    GIT_HASH = _git_repo.head.object.hexsha
+    try:
+        GIT_BRANCH_NAME = _git_repo.active_branch.name
+    except TypeError:
+        GIT_BRANCH_NAME = 'detached'
+    _describe_raw = _git_repo.git.describe(tags=True).split("-")  # like `v0.8.0-1-g000000`
+    GIT_DESCRIBE = _describe_raw[0]  # actual tag name like `v0.8.0`
+    if len(_describe_raw) > 1:
+        GIT_DESCRIBE += "." + _describe_raw[1]  # tag 之后的 commit 计数，代表小版本
+        # 最终结果类似于：v0.8.0.1
+
+    """
+    APM and error tracking platforms
+    """
     SENTRY_CONFIG = {
         'dsn'    : '',
         'release': '',
         'tags'   : {'environment': 'default'}
     }
     ELASTIC_APM = {
-        'SERVICE_NAME': 'everyclass-server',
-        'SECRET_TOKEN': 'token',
-        'SERVER_URL'  : 'http://127.0.0.1:8200',
+        'SERVICE_NAME'   : 'everyclass-server',
+        'SECRET_TOKEN'   : 'token',
+        'SERVER_URL'     : 'http://127.0.0.1:8200',
+        # https://www.elastic.co/guide/en/apm/agent/python/2.x/configuration.html#config-auto-log-stacks
+        'AUTO_LOG_STACKS': False,
+        'SERVICE_VERSION': GIT_DESCRIBE
     }
 
     """
@@ -72,18 +94,3 @@ class Config(object):
 
     # API
     API_CLIENTS = []
-
-    """
-    Git Hash
-    """
-    _git_repo = git.Repo(search_parent_directories=True)
-    GIT_HASH = _git_repo.head.object.hexsha
-    try:
-        GIT_BRANCH_NAME = _git_repo.active_branch.name
-    except TypeError:
-        GIT_BRANCH_NAME = 'detached'
-    _describe_raw = _git_repo.git.describe(tags=True).split("-")  # like `v0.8.0-1-g000000`
-    GIT_DESCRIBE = _describe_raw[0]  # actual tag name like `v0.8.0`
-    if len(_describe_raw) > 1:
-        GIT_DESCRIBE += "." + _describe_raw[1]  # tag 之后的 commit 计数，代表小版本
-        # 最终结果类似于：v0.8.0.1
