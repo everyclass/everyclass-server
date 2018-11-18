85ae61901a85b2912b343624c34e4576aab9fff9
Frederic Chan <frederic.t.chan@gmail.com>
2018-11-18 11:20:05 +0800

    use tag to check if need deploy, instead of branch
    Signed-off-by: Frederic Chan <frederic.t.chan@gmail.com>

--- deploy/travis.sh
+++ deploy/travis.sh
@@ -3,35 +3,31 @@
 
 set -xe
 
-if [ $TRAVIS_BRANCH = "release" ] ; then
-
-# checkout release first, otherwise `git describe` will be wrong
-git checkout release
-VERSION=$(git describe --tags)
-
-# sentry release
-curl -sL https://sentry.io/get-cli/ | bash
-export SENTRY_ORG=admirable
-export SENTRY_URL=https://sentry.admirable.pro/
-sentry-cli releases new -p everyclass-server --finalize "$VERSION"
-sentry-cli releases set-commits "$VERSION" --auto
-
-# build Docker image and upload to hub
-echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} ccr.ccs.tencentyun.com --password-stdin
-IMAGE_ADDRESS=ccr.ccs.tencentyun.com/everyclass/everyclass-server:${VERSION}
-docker build -t ${IMAGE_ADDRESS} .
-docker push ${IMAGE_ADDRESS}
-
-
-if [[ "$VERSION" == *_testing ]];then
-    sentry-cli releases deploys ${VERSION} new -e testing
+if [[ -z ${TRAVIS_TAG+x} ]]; then
+    echo "TRAVIS_TAG is unset. Skip deploying.";
 else
-    sentry-cli releases deploys ${VERSION} new -e staging
-fi
+    echo "TRAVIS_TAG is set to '$TRAVIS_TAG'. Start deploying.";
+    # checkout release first, otherwise `git describe` will be wrong
+    git checkout release
+    VERSION=$(git describe --tags)
+
+    # sentry release
+    curl -sL https://sentry.io/get-cli/ | bash
+    export SENTRY_ORG=admirable
+    export SENTRY_URL=https://sentry.admirable.pro/
+    sentry-cli releases new -p everyclass-server --finalize "$VERSION"
+    sentry-cli releases set-commits "$VERSION" --auto
+
+    # build Docker image and upload to hub
+    echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} ccr.ccs.tencentyun.com --password-stdin
+    IMAGE_ADDRESS=ccr.ccs.tencentyun.com/everyclass/everyclass-server:${VERSION}
+    docker build -t ${IMAGE_ADDRESS} .
+    docker push ${IMAGE_ADDRESS}
 
 
-else
-
-     echo "No deploy script for branch '$TRAVIS_BRANCH'. Skip deploying."
-
+    if [[ "$VERSION" == *_testing ]];then
+        sentry-cli releases deploys ${VERSION} new -e testing
+    else
+        sentry-cli releases deploys ${VERSION} new -e staging
+    fi
 fi
