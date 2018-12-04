2f6943274f73caee5205aabce924b685f02814c5
Frederic Chan <frederic.t.chan@gmail.com>
2018-12-04 19:09:35 +0800

    remove api v1
    Signed-off-by: Frederic Chan <frederic.t.chan@gmail.com>

--- everyclass/server/__init__.py
+++ everyclass/server/__init__.py
@@ -155,11 +155,9 @@ def create_app(outside_container=False) -> Flask:
     from everyclass.server.calendar.views import cal_blueprint
     from everyclass.server.query import query_blueprint
     from everyclass.server.views import main_blueprint as main_blueprint
-    from everyclass.server.api import api_v1 as api_blueprint
     app.register_blueprint(cal_blueprint)
     app.register_blueprint(query_blueprint)
     app.register_blueprint(main_blueprint)
-    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     @app.before_request
     def set_user_id():
--- everyclass/server/api/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-API related
-"""
-from flask import Blueprint
-
-api_v1 = Blueprint('api_v1', __name__)
-
-from everyclass.server.api.v1 import get_courses, get_semesters, error_handler_403
