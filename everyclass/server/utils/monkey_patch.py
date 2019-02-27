f85fe8382cac24b049533065c83175353b0188f5
Frederic <frederic.t.chan@gmail.com>
2019-02-27 12:52:54 +0800

    APM记录当前登录用户
    Signed-off-by: Frederic <frederic.t.chan@gmail.com>

--- everyclass/server/utils/monkey_patch.py
+++ everyclass/server/utils/monkey_patch.py
@@ -1,7 +1,9 @@
 import functools
 
 import elasticapm
-from flask import request, session
+from flask import session
+
+from everyclass.server.consts import SESSION_CURRENT_USER
 
 
 class ElasticAPM:
@@ -10,13 +12,13 @@ class ElasticAPM:
         @functools.wraps(original_func)
         def _patched(self, app, response):
             if not self.app.debug or self.client.config.debug:
-                # 在 context 中记录 cookie 中的 user id
+                # 在 context 的 user_id 中记录匿名 user id，在 username 中记录当前登录的用户学号
                 if session.get('user_id', None):
                     user_id = session['user_id']
-                else:
-                    user_id = request.cookies.get('UM_distinctid', None)
-                elasticapm.set_user_context(user_id=user_id,
-                                            username=session.get('stu_id', None))
+                    elasticapm.set_user_context(user_id=user_id)
+
+                if session.get(SESSION_CURRENT_USER, None):
+                    elasticapm.set_user_context(username=session[SESSION_CURRENT_USER])
 
             # execute the original `request_finished` function
             original_func(self, app, response)
