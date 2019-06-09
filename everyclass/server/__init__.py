915af6b6f755ee37fbd1568e3ed2c7b81643d68b
Frederic Chan <frederic.t.chan@gmail.com>
2019-06-09 21:17:02 +0800

    修改重启检测机制
    Signed-off-by: Frederic Chan <frederic.t.chan@gmail.com>

--- everyclass/server/__init__.py
+++ everyclass/server/__init__.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 
 import gc
@@ -15,7 +16,7 @@ from everyclass.server.utils import monkey_patch
 logger = logbook.Logger(__name__)
 sentry = Sentry()
 __app = None
-__first_spawn = True
+__load_time = datetime.datetime.now()
 
 
 try:
@@ -37,8 +38,6 @@ try:
         from elasticapm.contrib.flask import ElasticAPM
         from everyclass.server.config import print_config
         ElasticAPM.request_finished = monkey_patch.ElasticAPM.request_finished(ElasticAPM.request_finished)
-
-        global __app, __first_spawn
 
         # Elastic APM
         if __app.config['CONFIG_NAME'] in __app.config['APM_AVAILABLE_IN']:
@@ -63,14 +62,14 @@ try:
             logger.handlers.append(sentry_handler)
             print('Sentry is inited because you are in {} mode.'.format(__app.config['CONFIG_NAME']))
 
-        # print current configuration
+        # 如果当前时间与模块加载时间相差一分钟之内，认为是第一次 spawn（进程随着时间的推移可能会被 uwsgi 回收），
+        # 在 1 号 worker 里打印当前配置
         import uwsgi
-        if uwsgi.worker_id() == 1 and __first_spawn:
-            # set to warning level because we want to monitor restarts
+        if uwsgi.worker_id() == 1 and (datetime.datetime.now() - __load_time) < datetime.timedelta(minutes=1):
+            # 这里设置等级为 warning 因为我们希望在 sentry 里监控重启情况
             logger.warning('App (re)started in `{0}` environment'
                            .format(__app.config['CONFIG_NAME']), stack=False)
             print_config(__app)
-            __first_spawn = False
 
     @uwsgidecorators.postfork
     def init_db():
@@ -78,14 +77,12 @@ try:
         from everyclass.server.db.mongodb import init_pool as init_mongo
         from everyclass.server.db.postgres import init_pool as init_pg
 
-        global __app
         init_mongo(__app)
         init_pg(__app)
 
     @uwsgidecorators.postfork
     def init_session():
         """初始化服务器端 session"""
-        global __app
         __app.config['SESSION_MONGODB'] = __app.mongo
         Session(__app)
 
