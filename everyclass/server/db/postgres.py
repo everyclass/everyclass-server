bdaf47dc2a36d6b9d103186ae70c36bf7731e812
Frederic <frederic.t.chan@gmail.com>
2019-06-09 11:44:10 +0800

    try to fix pool exhausted error
    Signed-off-by: Frederic <frederic.t.chan@gmail.com>

--- everyclass/server/db/postgres.py
+++ everyclass/server/db/postgres.py
@@ -10,12 +10,29 @@ from everyclass.server.config import get_config
 _config = get_config()
 _options = f'-c search_path={_config.POSTGRES_SCHEMA}'
 
+from psycopg2.pool import ThreadedConnectionPool
+from threading import Semaphore
+
+
+class ReallyThreadedConnectionPool(ThreadedConnectionPool):
+    def __init__(self, minconn, maxconn, *args, **kwargs):
+        self._semaphore = Semaphore(maxconn)
+        super().__init__(minconn, maxconn, *args, **kwargs)
+
+    def getconn(self, *args, **kwargs):
+        self._semaphore.acquire()
+        return super().getconn(*args, **kwargs)
+
+    def putconn(self, *args, **kwargs):
+        super().putconn(*args, **kwargs)
+        self._semaphore.release()
+
 
 def init_pool(current_application) -> None:
     """创建连接池，保存在 app 的 postgres 属性中"""
-    current_application.postgres = psycopg2.pool.ThreadedConnectionPool(1, 10,
-                                                                        **_config.POSTGRES_CONNECTION,
-                                                                        options=_options)
+    current_application.postgres = ReallyThreadedConnectionPool(1, 10,
+                                                                **_config.POSTGRES_CONNECTION,
+                                                                options=_options)
 
 
 @contextmanager
