9cf77f7a4cf2a0f3c06dd0174bec96626c2fb1fe
Frederic <frederic.t.chan@gmail.com>
2019-06-10 15:22:40 +0800

    使用更健壮的连接池
    Signed-off-by: Frederic <frederic.t.chan@gmail.com>

--- everyclass/server/db/postgres.py
+++ everyclass/server/db/postgres.py
@@ -1,9 +1,8 @@
 from contextlib import contextmanager
-from threading import Semaphore
 
 import psycopg2
+from DBUtils.PooledDB import PooledDB
 from flask import current_app, has_app_context
-from psycopg2.pool import ThreadedConnectionPool
 
 from everyclass.server.config import get_config
 
@@ -11,41 +10,22 @@ _config = get_config()
 _options = f'-c search_path={_config.POSTGRES_SCHEMA}'
 
 
-class ReallyThreadedConnectionPool(ThreadedConnectionPool):
-    def __init__(self, minconn, maxconn, *args, **kwargs):
-        self._semaphore = Semaphore(maxconn)
-        super().__init__(minconn, maxconn, *args, **kwargs)
-
-    def getconn(self, *args, **kwargs):
-        self._semaphore.acquire()
-        return super().getconn(*args, **kwargs)
-
-    def putconn(self, *args, **kwargs):
-        super().putconn(*args, **kwargs)
-        self._semaphore.release()
-
-
 def init_pool(current_application) -> None:
     """创建连接池，保存在 app 的 postgres 属性中"""
-    current_application.postgres = ReallyThreadedConnectionPool(1, 10,
-                                                                **_config.POSTGRES_CONNECTION,
-                                                                options=_options)
+    current_application.postgres = PooledDB(creator=psycopg2,
+                                            mincached=1,
+                                            maxcached=3,
+                                            maxconnections=10,
+                                            **_config.POSTGRES_CONNECTION,
+                                            options=_options)
 
 
 @contextmanager
 def pg_conn_context():
     if has_app_context():
-        conn = current_app.postgres.getconn()
+        conn = current_app.postgres.connection()
     else:
         conn = psycopg2.connect(**_config.POSTGRES_CONNECTION,
                                 options=_options)
     yield conn
-    put_pg_conn(conn)
-
-
-def put_pg_conn(conn) -> None:
-    """将连接放回连接池"""
-    if has_app_context():
-        current_app.postgres.putconn(conn)
-    else:
-        conn.close()
+    conn.close()
