9637834e9f88a5dc7f4dac22bfa3867e9998235a
zhanzeyu.frederic <zhanzeyu.frederic@bytedance.com>
2020-05-13 23:20:11 +0800

    room API return encoded

--- everyclass/server/entity/model/rooms.py
+++ everyclass/server/entity/model/rooms.py
@@ -3,19 +3,21 @@ from typing import List, Dict
 
 from everyclass.rpc import ensure_slots
 from everyclass.server.utils import JSONSerializable
+from everyclass.server.utils.encryption import encrypt, RTYPE_ROOM
 
 
 @dataclass
 class Building(JSONSerializable):
     name: str
     rooms: List[str]
+    rooms_encoded: List[str]
 
     def __json_encode__(self):
-        return {'name': self.name, 'rooms': self.rooms}
+        return {'name': self.name, 'rooms_encoded': self.rooms_encoded}
 
     @classmethod
     def make(cls, name: str, rooms: List[str]) -> "Building":
-        dct_new = {"name": name, "rooms": rooms}
+        dct_new = {"name": name, "rooms": rooms, 'rooms_encoded': [encrypt(RTYPE_ROOM, x) for x in rooms]}
         return cls(**ensure_slots(cls, dct_new))
 
 
